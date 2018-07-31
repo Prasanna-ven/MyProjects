467af311d8a78afa4e730c99e5a8bd2aa643c0a8
prasanna <prasanna.ven@gmail.com>
2018-07-31 22:54:42 +0530

    python learning 31-07-2018

--- PycharmProjects/pythontut/odd.py
+++ PycharmProjects/pythontut/odd.py
@@ -1,8 +1,16 @@
-from datetime import datetime
+import datetime
+import os
+import sys
 
+cwd = os.getcwd()
+cos = sys.platform
+print(cwd)
+print(cos)
+print(sys.version)
+print(os.getenv('PATH'))
 odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49]
 
-right_this_minute = datetime.today().minute
+right_this_minute = datetime.datetime.today().minute
 if right_this_minute in odds:
     print('This minute looks odd')
 else:
