c934135be4c99b8e6284614bd89d04cb45884749
prasanna <prasanna.ven@gmail.com>
2018-08-15 07:47:53 +0530

    python learning

--- PycharmProjects/pythontut/odd.py
+++ PycharmProjects/pythontut/odd.py
@@ -1,17 +1,40 @@
 import datetime
 import os
 import sys
+import time
 
-cwd = os.getcwd()
-cos = sys.platform
-print(cwd)
-print(cos)
+print(datetime.date.today())
+print(time.strftime('%A %H : %M'))
+print(os.getcwd())
+print(sys.platform)
 print(sys.version)
 print(os.getenv('PATH'))
-odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49]
+odds = [1, 3, 5, 7, 9, 11, 13, 15, 17, 19, 21, 23, 25, 27, 29, 31, 33, 35, 37, 39, 41, 43, 45, 47, 49, 51, 53, 55, 57, 59]
 
 right_this_minute = datetime.datetime.today().minute
+right_this_hour = datetime.datetime.today().hour
+right_this_day = datetime.datetime.today().day
+right_this_month = datetime.datetime.today().month
+right_this_year = datetime.datetime.today().year
+right_this = datetime.datetime.today().date().isoformat()
 if right_this_minute in odds:
     print('This minute looks odd')
 else:
-    print('Not a odd minute')
+    print('Not an odd minute')
+if right_this_hour in odds:
+    print('This is an odd hour')
+else:
+    print('this is not an odd hour')
+if right_this_day in odds:
+    print('This is an odd day')
+else:
+    print('this is not an odd day')
+if right_this_month in odds:
+    print('This is an odd month')
+else:
+    print('this is not an odd month')
+if right_this_year%2 in odds:
+    print('This is an odd year')
+else:
+    print('this is not an odd year')
+print(right_this)
